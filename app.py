16f446ff814505119652d5c4d27bf4cf552c457a
SHYAM YUVAKAR SUNDARAMOORTHY <h2610135@nushigh.edu.sg>
2026-07-15 09:35:21 +0800

    Prepare app for Render deployment

--- app.py
+++ app.py
@@ -110,12 +110,15 @@ def home():
 # Run Website
 # ==========================================
 
+import os
+
 if __name__ == "__main__":
 
     print("🚀 Starting Flask server...")
 
+    port = int(os.environ.get("PORT", 5000))
+
     app.run(
-        host="127.0.0.1",
-        port=5000,
-        debug=True
+        host="0.0.0.0",
+        port=port
     )
